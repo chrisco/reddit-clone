844ad31f1eeb290ef4a2ecf16df084b39f3159f6
Christopher Comella <git.chrisco@gmail.com>
2016-07-23 18:57:27 -0600

    Deliver v1 of "Create New Post Feature" modified:   app/controllers.js modified:   app/posts.js modified:   index.html

--- app/controllers.js
+++ app/controllers.js
@@ -1,3 +1,45 @@
 function RedditController($scope) {
-  $scope.posts = posts;
+	// Get the array of posts:
+	$scope.posts = posts;
+
+	// Don't show the newPostForm initially:
+	$scope.showNewPostForm = false;
+
+	// Toggle it as needed:
+	$scope.toggleNewPostForm = function() {
+		if ($scope.showNewPostForm === true) {
+			$scope.showNewPostForm = false;
+		} else {
+			$scope.showNewPostForm = true;
+		}
+	};
+
+	$scope.createNewPost = function() {
+		// Create the new post:
+		var newPost = {};
+		newPost.title = $scope.newPostForm.title;
+		newPost.author = $scope.newPostForm.author;
+		newPost.imageURL = $scope.newPostForm.imageURL;
+		newPost.postBody = $scope.newPostForm.postBody;
+		newPost.timestamp = new Date();
+		newPost.points = 0;
+		newPost.addPoints = function() {
+			this.points += 1;
+		};
+		newPost.subtractPoints = function() {
+			this.points -= 1;
+		};
+		newPost.commentCount = 0;
+		newPost.comments = [];
+
+		// Push it to the array of posts:
+		posts.push(newPost);
+
+		// Reset the form:
+		$scope.newPostForm.title = "";
+		$scope.newPostForm.author = "";
+		$scope.newPostForm.imageURL = "";
+		$scope.newPostForm.postBody = "";
+		$scope.showNewPostForm = false;
+	};
 }
--- app/posts.js
+++ app/posts.js
@@ -2,7 +2,7 @@ var posts = [{
 	title: "Longs Peak",
 	author: "Alice",
 	imageURL: "http://imagesofrmnp.com/images/large/Longs-Peak-Autumn.jpg",
-	body: "Longs Peak is a high and prominent mountain summit in the northern Front Range of the Rocky Mountains of North America. The 14,259-foot (4346 m) fourteener is located in the Rocky Mountain National Park Wilderness, 9.6 miles (15.5 km) southwest by south (bearing 209°) of the Town of Estes Park, Colorado.",
+	postBody: "Longs Peak is a high and prominent mountain summit in the northern Front Range of the Rocky Mountains of North America. The 14,259-foot (4346 m) fourteener is located in the Rocky Mountain National Park Wilderness, 9.6 miles (15.5 km) southwest by south (bearing 209°) of the Town of Estes Park, Colorado.",
 	timestamp: "2016-07-20T14:50:26-06:00",
 	points: 0,
 	addPoints: this.points += 1,
@@ -19,7 +19,7 @@ var posts = [{
 	title: "Pikes Peak",
 	author: "Bob",
 	imageURL: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Pikes_Peak_from_Garden_of_the_Gods.JPG",
-	body: "Pikes Peak is the highest summit of the southern Front Range of the Rocky Mountains of North America. The ultra-prominent 14,115-foot (4,302.31 m) fourteener is located in Pike National Forest, 12.0 miles (19.3 km) west by south (bearing 263°) of downtown Colorado Springs in El Paso County, Colorado.",
+	postBody: "Pikes Peak is the highest summit of the southern Front Range of the Rocky Mountains of North America. The ultra-prominent 14,115-foot (4,302.31 m) fourteener is located in Pike National Forest, 12.0 miles (19.3 km) west by south (bearing 263°) of downtown Colorado Springs in El Paso County, Colorado.",
 	timestamp: "2016-07-20T14:50:26-06:00",
 	points: 0,
 	addPoints: this.points += 1,
@@ -27,9 +27,9 @@ var posts = [{
 	commentCount: 2,
 	comments: [{
 		commenter: "Alice",
-		comment: "Spiffy!"
+		commentBody: "Spiffy!"
 	}, {
 		commenter: "John",
-		comment: "I like it!"
+		commentBody: "I like it!"
 	}]
 }];
